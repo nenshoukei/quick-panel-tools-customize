9879eaa2300c32b346e2e35fdf7d28bba69e92c5
nenshoukei <mokkosu.nico@gmail.com>
2026-02-28 12:42:28 +0900

    Remove debug code

--- control.lua
+++ control.lua
@@ -2,16 +2,4 @@ local event_handler = require("__core__.lualib.event_handler")
 
 event_handler.add_libraries({
   require("scripts.control.customize-gui-control"),
-  {
-    on_init = function ()
-      if script.active_mods["debugadapter"] then
-        -- For debugging: Skip intro and crashsite
-        local freeplay = remote.interfaces["freeplay"]
-        if freeplay then
-          if freeplay["set_skip_intro"] then remote.call("freeplay", "set_skip_intro", true) end
-          if freeplay["set_disable_crashsite"] then remote.call("freeplay", "set_disable_crashsite", true) end
-        end
-      end
-    end,
-  },
 })
